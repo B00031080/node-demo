1000714636a58cae95b3696fcddcc73defa4337a
B00031080 <B00031080@essec.edu>
2018-03-14 18:59:10 +0100

    Use database

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongo = require('mongodb');
 const app = express();
 
-app.use(express.static('static'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.get('/help', (req, res) => res.send('Coding is hard!'));
+const url = "mongodb://localhost:27017/";
 
-app.get('/', (req, res) => res.send('Hello World!'));
+mongo.MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("essec");
+  
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+    app.use(express.static('static'))
+
+    app.get('/help', (req, res) => res.send('Coding is hard!'));
+
+    app.get('/students/:age', (req, res) => {
+        dbo.collection("students").find({age:{$gt:parseInt(req.params.age)}}).toArray(function(err, result) {
+            if (err) throw err;
+            res.render('students',{students:result});
+          });
+    });
+
+    app.get('/', (req, res) => res.send('Hello World!'));
+
+    app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+});
+
+
